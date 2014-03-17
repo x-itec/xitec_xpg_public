e2b87485e54c327b09cee7907135efeaff7c9088
Boris Koester <xitec75@googlemail.com>
2014-03-17 18:55:45 +0100

    readme init

--- xpg_public.rb
+++ xpg_public.rb
@@ -106,10 +106,10 @@ if you use it for encryption.
 
   Now let's encrypt a file with this very long password
 
-  openssl enc -aes-256-cbc -a -salt -in infile.txt -out outfile.enc
-          -pass pass:`xpg genpass --pass 'mysecretpassword' --service 'openssl-aes' -x`
+  openssl enc -aes-256-cbc -a -salt -in infile.txt -out outfile.enc \
+  -pass pass:`xpg genpass --pass 'mysecretpassword' --service 'openssl-aes' -x`
 
-  The attacker cannot decrypt the file with 
+  The attacker cannot decrypt the file with
   'mysecretpassword' he needs this tool AND your correct pass + service-name.
 
     Let's test the decryption without the tool to figure out if it works.
@@ -123,25 +123,45 @@ EypIf5K7rHxbkWNXupMA6taWqGX7aRq8VC1X1wIBYKmjxhayP
 kv3Ko27BGQFRYWZzTwy42L6LBBpYjKad8LfjC29zAX5omnRZSU
 yxhg5Q8AtqKCMEzNbMc128qAlYojdOVW -out decrypted.txt
 
-  Yeah - but we just need this for decryption and your original password is still ultra secure and never used. So you are protected against dictionary-attacks, brute-force attacks and more. This password is too long and specially designed to be secure.
+  Yeah - but we just need this for decryption and your original password
+ is still ultra secure and never used. So you are protected against
+ dictionary-attacks, brute-force attacks and more.
+This password is too long and specially designed to be secure.
 
-  openssl enc -d -aes-256-cbc -a -in outfile.enc 
-          -pass pass:`xpg genpass --pass 'mysecretpassword' --service 'openssl-aes' -x` -out decrypted.txt	
+  openssl enc -d -aes-256-cbc -a -in outfile.enc \
+          -pass pass:`xpg genpass --pass 'mysecretpassword'
+              --service 'openssl-aes' -x` -out decrypted.txt
 
 REASON
 ------
-Everybody wants your password - give them a pass, but its never the same per service and not your real password. Additionally, its protected in a way that it's not available as a dictionary pass. 
-
+Everybody wants your password - give them a pass, but its never the same
+per service and not your real password. Additionally, its protected in
+a way that it's not available as a dictionary pass.
 
 FINALLY
 -------
 
-The idea or reason for this tool is simple. Passwords are insecure but no-one wants to remember passwords for every silly site or service. So you can still use ONE password for everything you want to use AND you can use this script to use it with other security-tools / batch-operations to protect your data.
+The idea or reason for this tool is simple. Passwords are insecure but no-one
+wants to remember passwords for every silly site or service. So you can still
+use ONE password for everything you want to use AND you can use this
+script to use it with other security-tools / batch-operations
+to protect your data.
 
-To generate ultra-strong passwords for encryption-services like openssl,openvpn, disk-encryption or other systems with 'unlimited' passwords, use -x or --strong to get a really long password thats ULTRA SECURE but too big for a typical website. 
+To generate ultra-strong passwords for encryption-services like openssl,
+openvpn, disk-encryption or other systems with 'unlimited' passwords,
+use -x or --strong to get a really long password thats ULTRA SECURE
+but too big for a typical website.
+
 HOW DOES THIS WORK?
 -------------------
-Your password is recalculated in a very complex procedure more than 200.000 times. The result is binary crap and converted into human readable form. This is not a toy, you can really rely on this tool and convert an easy password into something thats nearly unbreakable (see -x option). No one can figure out your primary password, because you need the pass AND the service name. Questions like 'he might using this or that' are now history, your pass is garbage. 
+Your password is recalculated in a very complex procedure
+more than 200.000 times. The result is binary crap
+and converted into human readable form. This is not a toy, you can really rely
+on this tool and convert an easy password into something
+thats nearly unbreakable (see -x option). No one can figure out
+your primary password, because you need the pass
+AND the service name. Questions like 'he might using this or that'
+are now history, your pass is garbage.
 
 WHY?
 ----
@@ -154,30 +174,29 @@ Boris Koester; http://www.x-itec.net
   "
   app.action do |options, arguments|
     #    ExampleCommand.new(options, arguments).execute
- end
+  end
 
- app.command :global, :aliases => :glob  do |command|
+  app.command :global, :aliases => :glob  do |command|
     #	puts "enter --help"
   end
 
-app.command :genpass do |command|
+  app.command :genpass do |command|
     command.options do |opts|
-      opts.opt :password, "Initial Password", :short => '-p', :long => '--pass', :type => :string, :default => ""
-      opts.opt :salt, "Service, i. e. 'facebook,xing,github,...'", :short => '-s', :long => '--service', :type => :string, :default => "x-itec"
-      opts.opt :rounds, "Birthdate, format: ttmmyyyy", :short => "-r", :long => '--rounds', :type => :integer, :default => 210375
-      opts.opt :flag_strong, "STRONG - create long password for special operations", :short => '-x', :long => '--strong', :type => :boolean
+      opts.opt :password, 'Initial Password', :short => '-p', :long => '--pass', :type => :string, :default => ''
+      opts.opt :salt, 'Service, i. e. facebook,xing,github,...', :short => '-s', :long => '--service', :type => :string, :default => 'x-itec'
+      opts.opt :rounds, 'Birthdate, format: ttmmyyyy', :short => '-r', :long => '--rounds', :type => :integer, :default => 210375
+      opts.opt :flag_strong, 'STRONG - create long password for special operations', :short => '-x', :long => '--strong', :type => :boolean
 
-   end
+    end
     command.action do |options, arguments|
-     #	 ExampleCommand.new(options, arguments).execute
+      #	 ExampleCommand.new(options, arguments).execute
 
       PassGenerator.new(options, arguments).execute
     end
- end
+  end
 end
 
-
-#key = OpenSSL::PKCS5.pbkdf2_hmac_sha1("Ich bin das Kennwort und absolut supercool".reverse, "x-itec", 210375, 32)
-#puts key.unpack("H*")[0].to_i(16).to_s(32)
-#puts key.unpack("H*")[0].to_i(16).base62_encode.reverse
-#puts key.unpack("H*").pack('H*')
+# key = OpenSSL::PKCS5.pbkdf2_hmac_sha1("Ich bin das Kennwort".reverse, "x-itec", 210375, 32)
+# puts key.unpack("H*")[0].to_i(16).to_s(32)
+# puts key.unpack("H*")[0].to_i(16).base62_encode.reverse
+# puts key.unpack("H*").pack('H*')
